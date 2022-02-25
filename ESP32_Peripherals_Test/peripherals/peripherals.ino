94ae9ba1824d7f196ffd22b2d47946e528d05c45
Sedona Thomas <sntsregister@gmail.com>
2022-02-25 14:02:33 -0500

    Test for button, joystick, potentiometer

---
+++ ESP32_Peripherals_Test/peripherals/peripherals.ino
@@ -0,0 +1,41 @@
+/*
+ * This is an example how to use Touch Intrrerupts and read touch values
+ * 
+ * @author Professor Mark Santolucito
+ * @link https://gist.github.com/santolucito/44410ed78def1b68b9994b74227f59ee
+ */
+
+int threshold = 40;
+bool touch1detected = false;
+bool touch2detected = false;
+uint8_t touch1Val = 0;
+uint8_t touch2Val = 0;
+
+void gotTouch1(){
+ touch1detected = true;
+ touch1Val = touchRead(T2);
+}
+
+void gotTouch2(){
+ touch2detected = true;
+ touch2Val = touchRead(T9);
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000); // give me time to bring up serial monitor
+  Serial.println("ESP32 Touch Interrupt Test");
+  touchAttachInterrupt(T2, gotTouch1, threshold);
+  touchAttachInterrupt(T9, gotTouch2, threshold);
+}
+
+void loop(){
+  if(touch1detected){
+    touch1detected = false;
+    Serial.println(touch1Val);
+  }
+  if(touch2detected){
+    touch2detected = false;
+    Serial.println(touch2Val);
+  }
+}
---
+++ ESP32_Peripherals_Test/reader.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+  this code will read the touch values from the ESP32 and echo them on the command line
+  you could do something else more interesting with these values (e.g. visualize/sonify)
+  @author Professor Mark Santolucito
+  @link https://gist.github.com/santolucito/44410ed78def1b68b9994b74227f59ee
+'''
+
+import serial
+
+esp32_port = '/dev/cu.usbserial-023E564D'
+
+ser = serial.Serial(esp32_port, 115200)
+
+while(True):
+  print(str(ser.readline().strip(), 'ascii'))
+
