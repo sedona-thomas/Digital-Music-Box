44066de4648c8352072438a19a21a8fe2ddc4193
Sedona Thomas <sntsregister@gmail.com>
2022-02-25 16:06:55 -0500

    Edited wrong file

--- ESP32_Peripherals_Test/reader.py
+++ ESP32_Peripherals_Test/reader.py
@@ -1,19 +1,52 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-'''
-  this code will read the touch values from the ESP32 and echo them on the command line
-  you could do something else more interesting with these values (e.g. visualize/sonify)
-  @author Professor Mark Santolucito
-  @link https://gist.github.com/santolucito/44410ed78def1b68b9994b74227f59ee
-'''
-
 import serial
 
-esp32_port = '/dev/cu.usbserial-023E564D'
+port = '/dev/cu.usbserial-023E564D'  # esp32
+baudrate = 115200
 
-ser = serial.Serial(esp32_port, 115200)
+s = serial.Serial(port, baudrate)
 
 while(True):
-  print(str(ser.readline().strip(), 'ascii'))
+    print(get_sensor_values())
 
+    # button = get_button()
+    # potentiometer = get_potentiometer()
+    # joystick = get_joystick()
+
+    # print("current values:")
+    # print(button)
+    # print(potentiometer)
+    # print(joystick)
+
+
+def get_sensor_value():
+    sensor = s.readline().strip()
+    return str(sensor, 'ascii')
+
+
+def get_button():
+    button = {"-": 0}
+    button_pins = {"-": 12}
+
+    return button
+
+
+def get_potentiometer():
+    potentiometer = {"in": 0, "out": 0, "ground": 0}
+    potentiometer_pins = {"out": 13}
+
+    return potentiometer
+
+
+def get_joystick():
+    joystick = {"ground": 0, "5V": 0, "VRx": 0, "VRy": 0, "SW": 0}
+    joystick_pins = {"VRx": 27, "VRy": 26, "SW": 25}
+
+    return joystick
+
+# setup
+# button = {"+": "3V", "-" : "12"}
+# potentiometer = {"in": "3V", "out": "13", "ground": "G"}
+# joystick = {"ground": "G", "5V": "5V", "VRx": "27", "VRy": "26", "SW": "25"}
