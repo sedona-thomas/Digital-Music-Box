a246e052edb5c847b217f9930c73f4a976e4f7b6
Sedona Thomas <sntsregister@gmail.com>
2022-03-03 10:27:21 -0500

    Update device.ino

--- Esp32_Interactive Device/device/device.ino
+++ Esp32_Interactive Device/device/device.ino
@@ -23,6 +23,7 @@ Peripheral(std::string _name, int _pin) {
 
 // read(): reads peripheral value
 void Peripheral::read() {
+  detected = true;
   pinMode(pin, INPUT_PULLUP);
   digitalRead(pin);
 };
@@ -87,9 +88,112 @@ void PeripheralTouch::send() {
 //
 //
 
+class Button {
+public:
+  const uint8_t pin;
+  bool detected;
+  uint8_t value;
+
+  Button(std::string, int);
+  void read();
+  void send();
+};
+
+Button(int _pin) {
+  pin = _pin;
+  detected = false;
+  value = 0;
+}
+
+// read(): reads button value
+void Peripheral::read() {
+  detected = true;
+  pinMode(pin, INPUT_PULLUP);
+  digitalRead(pin);
+};
+
+// send(): sends data from peripheral over the serial connection
+void Button::send() {
+  if (detected) {
+    detected = false;
+    Serial.print("<button>");
+    Serial.print(value);
+    Serial.print("</button>");
+  }
+};
+
+class Potentiometer {
+public:
+  const uint8_t pin;
+  bool detected;
+  uint8_t value;
+
+  Button(std::string, int);
+  void read();
+  void send();
+};
+
+Potentiometer(int _pin) {
+  pin = _pin;
+  detected = false;
+  value = 0;
+}
+
+// read(): reads potentiometer value
+void Potentiometer::read() {
+  detected = true;
+  analogRead(pin);
+};
+
+// send(): sends data from peripheral over the serial connection
+void Potentiometer::send() {
+  if (detected) {
+    detected = false;
+    Serial.print("<potentiometer>");
+    Serial.print(value);
+    Serial.print("</potentiometer>");
+  }
+};
+
+class Joystick {
+public:
+  Potentiometer potentiometerX;
+  Potentiometer potentiometerY;
+  Button buttonSW;
+
+  Button(std::string, int);
+  void read();
+  void send();
+};
+
+Joystick(std::string _name, int _pinX, int _pinY, int _pinSW) {
+  potentiometerX = Potentiometer(_pinX);
+  potentiometerY = Potentiometer(_pinY);
+  buttonSW = Button(_pinSW);
+}
+
+// read(): reads joystick value
+void Joystick::read() {
+  potentiometerX.read();
+  potentiometerY.read();
+  buttonSW.read();
+};
+
+// send(): sends data from peripheral over the serial connection
+void Joystick::send() {
+  Serial.print("<joystick>");
+  potentiometerX.send();
+  potentiometerY.send();
+  buttonSW.send();
+  Serial.print("</joystick>");
+};
+
+/*
+ * main code
+ */
+
 int threshold = 40;
 
-// main
 void setupSerial();
 void setupPeripherals();
 int readPin(int pin);
