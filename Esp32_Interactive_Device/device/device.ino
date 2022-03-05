c57919aba1a4069835f13c54d95698b7b76377e4
Sedona Thomas <sntsregister@gmail.com>
2022-03-05 17:20:11 -0500

    Value queue moved to separate file

--- Esp32_Interactive_Device/device/device.ino
+++ Esp32_Interactive_Device/device/device.ino
@@ -7,26 +7,12 @@
 #define JSON true           // sends JSON data over serial connection not tagged
 
 #include "esp32_screen.h"
+#include "value_queue.h"
 #include <SPI.h>
 #include <TFT_eSPI.h>
 #include <list>
 #include <stdint.h>
 #include <string>
-
-class ValueQueue {
-private:
-  std::list<uint8_t> values;
-
-public:
-  ValueQueue();
-  ValueQueue(int);
-  inline void add(uint8_t value);
-  void push(uint8_t value);
-  void pop();
-  inline uint8_t contains(int i);
-  uint8_t average();
-  inline size_t size();
-};
 
 class Sensor {
 protected:
@@ -114,43 +100,6 @@ void sendPeripherals() {
     Serial.println("</data>");
   }
 }
-
-////////////////////////////////////////////////////////////////////////////////
-
-ValueQueue::ValueQueue() {
-  for (int i = 0; i < 5; i++) {
-    values.push_back(0);
-  }
-}
-
-ValueQueue::ValueQueue(int size) {
-  for (int i = 0; i < size; i++) {
-    values.push_back(0);
-  }
-}
-
-void ValueQueue::add(uint8_t value) {
-  push(value);
-  pop();
-}
-
-inline void ValueQueue::push(uint8_t value) { values.push_back(value); }
-
-inline void ValueQueue::pop() { values.pop_front(); }
-
-inline uint8_t ValueQueue::contains(int i) {
-  return (std::find(values.begin(), values.end(), i) != values.end());
-}
-
-uint8_t ValueQueue::average() {
-  int sum = 0;
-  for (auto val : values) {
-    sum += val;
-  }
-  return sum / size();
-}
-
-inline size_t ValueQueue::size() { return values.size(); }
 
 ////////////////////////////////////////////////////////////////////////////////
 
---
+++ Esp32_Interactive_Device/device/value_queue.h
@@ -0,0 +1,19 @@
+#ifndef __VALUE_QUEUE_H__
+#define __VALUE_QUEUE_H__
+
+class ValueQueue {
+private:
+  std::list<uint8_t> values;
+
+public:
+  ValueQueue();
+  ValueQueue(int);
+  inline void add(uint8_t value);
+  void push(uint8_t value);
+  void pop();
+  inline uint8_t contains(int i);
+  uint8_t average();
+  inline size_t size();
+};
+
+#endif
