7263b8d2941053c544cea7faf909db93e068f97a
Sedona Thomas <sntsregister@gmail.com>
2022-03-05 00:03:16 -0500

    Making class .h and .cpp files

---
+++ Esp32_Interactive_Device/device/Button.h
@@ -0,0 +1,19 @@
+#ifndef __BUTTON_H__
+#define __BUTTON_H__
+
+class Button {
+public:
+  std::string name;
+  uint8_t pin;
+  uint8_t value;
+  std::list<uint8_t> values;
+  bool json;
+
+  Button() {}
+  Button(std::string, int, bool);
+  Button(int, bool);
+  void read();
+  void send();
+};
+
+#endif
--- Esp32_Interactive_Device/device/device.ino
+++ Esp32_Interactive_Device/device/device.ino
@@ -2,6 +2,7 @@
  *
  */
 
+#include "button.h"
 #include <list>
 
 #define WAIT 500     // miliseconds
@@ -28,76 +29,6 @@ unsigned long loopStartTime = 0; // Used for testing draw times
 /*
  * Classes
  */
-
-class Button {
-public:
-  std::string name;
-  uint8_t pin;
-  uint8_t value;
-  std::list<uint8_t> values;
-  bool json;
-
-  Button() {}
-  Button(std::string, int, bool);
-  Button(int, bool);
-  void read();
-  void send();
-};
-
-Button::Button(std::string name_in, int pin_in, bool json_in) {
-  name = name_in;
-  pin = pin_in;
-  value = 0;
-  json = json_in;
-  for (int i = 0; i < 8; i++) {
-    values.push_back(0);
-  }
-}
-
-Button::Button(int pin_in, bool json_in) {
-  name = "";
-  pin = pin_in;
-  value = 0;
-  json = json_in;
-  for (int i = 0; i < 8; i++) {
-    values.push_back(0);
-  }
-}
-
-// read(): reads button value
-void Button::read() {
-  pinMode(pin, INPUT_PULLUP);
-  values.push_back(digitalRead(pin));
-  values.pop_front();
-  int sum = 0;
-  for (auto val : values) {
-    sum += val;
-  }
-  value = sum / values.size() < 0.1 ? 0 : 1;
-  value = (std::find(values.begin(), values.end(), 1) != values.end());
-  tft.println("button");
-  tft.println(value);
-};
-
-// send(): sends data from peripheral over the serial connection
-void Button::send() {
-  Button::read();
-  if (json) {
-    Serial.print("button_");
-    Serial.print(name.c_str());
-    Serial.print(": ");
-    Serial.print(value);
-    Serial.print(",");
-  } else {
-    Serial.print("<button_");
-    Serial.print(name.c_str());
-    Serial.print(">");
-    Serial.print(value);
-    Serial.print("</button_");
-    Serial.print(name.c_str());
-    Serial.print(">");
-  }
-};
 
 class Potentiometer {
 public:
